7302bc5d71820c54490dde4bcdc2c94bb3191689
Jose Moreira <jose.antonio.moreira18@gmail.com>
2024-09-24 17:03:40 +0100

    Closest Neighbours

---
+++ Java/ClosestNeighbours/ClosestNeighbours.java
@@ -0,0 +1,21 @@
+public class ClosestNeighbours
+{
+	public static void main(String[] args )
+	{
+		int[] numbers = {0,5,1209,6,2,111,112,33};
+		int smallestNumber = 0;
+		int[] smallestNeighbours = new int[2];
+
+		for(int i=0; i< numbers.length-1; i++)
+		{
+			int neighboursDifference = Math.abs(numbers[i]-numbers[i+1]);
+			if(neighboursDifference < smallestNumber || i == 0)
+			{
+				smallestNumber = neighboursDifference;
+				smallestNeighbours[0] = numbers[i];
+				smallestNeighbours[1] = numbers[i+1];
+			}
+		}
+		System.out.println(smallestNeighbours[0] + " " + smallestNeighbours[1]);
+	}
+}
