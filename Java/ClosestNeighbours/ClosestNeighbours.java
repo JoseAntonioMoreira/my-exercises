a897729ac62440ec98286c8b3e65a3fe5fefaaa7
Jose Moreira <jose.antonio.moreira18@gmail.com>
2024-09-25 10:24:58 +0100

    Optimization of ClosestNeighbours

--- Java/ClosestNeighbours/ClosestNeighbours.java
+++ Java/ClosestNeighbours/ClosestNeighbours.java
@@ -3,13 +3,13 @@ public class ClosestNeighbours
 	public static void main(String[] args )
 	{
 		int[] numbers = {0,5,1209,6,2,111,112,33};
-		int smallestNumber = 0;
+		int smallestNumber = Integer.MAX_VALUE;
 		int[] smallestNeighbours = new int[2];
 
 		for(int i=0; i< numbers.length-1; i++)
 		{
 			int neighboursDifference = Math.abs(numbers[i]-numbers[i+1]);
-			if(neighboursDifference < smallestNumber || i == 0)
+			if(neighboursDifference < smallestNumber)
 			{
 				smallestNumber = neighboursDifference;
 				smallestNeighbours[0] = numbers[i];
