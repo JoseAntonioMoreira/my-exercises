64480fffaf68702e8efa306aeb6bb2c74121d6ec
JoseAntonioMoreira <52548533+JoseAntonioMoreira@users.noreply.github.com>
2024-10-30 00:22:06 +0000

    Link to presentation

--- Java/season-2/TCP/ChatClient/src/Client.java
+++ Java/season-2/TCP/ChatClient/src/Client.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
+//https://www.canva.com/design/DAGU_7dt7dk/1AbJAeJ9m_aBNx-f_COuQw/edit
 public class Client {
     String hostName = "localhost";
     int portNumber = 50000;
